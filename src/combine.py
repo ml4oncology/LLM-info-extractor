e4ed7ed3272befc82585e24f420fcf3c740e4ae6
KevinJCHe <kevinjc.he@gmail.com>
2025-06-26 15:49:09 -0400

    include column name for fixed failed output

--- src/combine.py
+++ src/combine.py
@@ -6,17 +6,16 @@ import glob
 import json
 
 import pandas as pd
-
 from ml_common.util import load_table, save_table
 
 
-def fix_failed_output(df: pd.DataFrame):
-    if 'failed_output' not in df:
+def fix_failed_output(df: pd.DataFrame, failed_output_col: str = 'failed_output') -> pd.DataFrame:
+    if failed_output_col not in df:
         return df
-    
-    mask = df['failed_output'].notnull()
+
+    mask = df[failed_output_col].notnull()
     fixed_output = []
-    for generated_text in df.loc[mask, 'failed_output']:
+    for generated_text in df.loc[mask, failed_output_col]:
         if generated_text.endswith('"'):
             # reached max token 
             generated_text += '}'
@@ -25,19 +24,22 @@ def fix_failed_output(df: pd.DataFrame):
             # reached max token 
             generated_text += '"}'
 
-        # replace double quotes in reasoning with single quote
-        start_idx = generated_text.index('"Reason": "') + len('"Reason": "')
-        end_idx = -3
-        generated_text = (
-            generated_text[:start_idx] 
-            + generated_text[start_idx:end_idx].replace('"', '\'') 
-            + generated_text[end_idx:]
-        )
+        if generated_text.startswith('{'):
+            # replace double quotes in reasoning with single quote
+            start_idx = generated_text.find('"Reason": "') 
+            if start_idx != -1:
+                start_idx += len('"Reason": "')
+                end_idx = -3
+                generated_text = (
+                    generated_text[:start_idx] 
+                    + generated_text[start_idx:end_idx].replace('"', '\'') 
+                    + generated_text[end_idx:]
+                )
 
         try:
             result = json.loads(generated_text)
         except json.JSONDecodeError:
-            result = {'failed_output': generated_text}
+            result = {failed_output_col: generated_text}
         fixed_output.append(result)
     
     fixed_output = pd.DataFrame(fixed_output, index=df.index[mask])
